e83120afc08cc44e95c3138aaf5bf10a4b44a53a
Terrell-Alves <terrell.alves@mscoding.org>
2025-06-09 11:11:12 -0500

    javascript changes
    Setting the win condition for the game

--- JS/app.js
+++ JS/app.js
@@ -3,11 +3,9 @@ let reset = document.querySelector('.reset');
 let board = document.querySelector('.board');
 let squares = document.querySelectorAll('.squares');
 let text = document.querySelector('.text');
-let players = ['x', 'o'];
+let players = ['X', 'O'];
 let win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-let spots = [0 ,1 , 2];
-console.log(win[0] == spots);
-console.log(spots);
+let spots = ['' ,'' , '', '', '', '', '', '', ''];
 
 board.style.display = 'none';
 reset.style.display = 'none';
@@ -16,42 +14,47 @@ start.addEventListener('click', () => {
     board.style.display = 'grid';
     reset.style.display = 'block';
     start.style.display = 'none';
-    players = 'x';
+    players = 'X';
+    text.innerText = "X's Turn";
+    let winner = false
+
 
     for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener('click', () => {
-            if (players == 'x') {
+            if (players === 'X') {
                 let x = document.createElement('p');
                 x.classList.add('x');
                 squares[i].appendChild(x);
-                x.innerText = 'x';
-                players = 'o';
+                x.textContent = 'x';
+                players = 'O';
                 squares[i].style.backgroundColor = 'gray';
-            
-                for(let i = 0; i < win.length; i++) {
-                    for(let j = 0; j < spots.length; j++) {
-                        // console.log(spots[j])
-                        // console.log(win[i][j])
-                        // console.log(spots[j] == win[i][j]);
-                        
-                    }
-                    // How to incorporate the player variable into for loop
-                }
+                text.innerText = `${players}'s Turn`;
             } else {
                 let o = document.createElement('p');
                 o.classList.add('o');
                 squares[i].appendChild(o);
-                o.innerText = 'o';
-                players = 'x';
+                o.textContent = 'o';
+                players = 'X';
                 squares[i].style.backgroundColor = 'gray';
+                text.innerText = `${players}'s Turn`;
             }
         })
     }
 
-     if (players == 'x') {
-        text.innerText = "X's Turn"
-    } else {
-        text.innerText = "O's Turn"
+    for(let i = 0; i < win.length; i++) {
+        let winConditon = win[i];
+        let square1 = spots[winConditon[0]]
+        let square2 = spots[winConditon[1]]
+        let square3 = spots[winConditon[2]]
+
+        if(square1 == square2 && square2 == square3) {
+            let winner = true
+            break;
+        }
+    }
+
+    if(winner == true) {
+        text.textContent = `${players} wins!`;
     }
 })
     
