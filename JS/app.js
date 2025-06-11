e58163c406b11fc24761c85aa797032b43fab74e
Terrell-Alves <terrell.alves@mscoding.org>
2025-06-11 12:57:57 -0500

    javascript changes
    Able to decide a winner

--- JS/app.js
+++ JS/app.js
@@ -5,8 +5,7 @@ let squares = document.querySelectorAll('.squares');
 let text = document.querySelector('.text');
 let players = 'X';
 let win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-// let spots = ['' ,'' , '', '', '', '', '', '', ''];
-let endGame = false;
+let spots = ['' ,'' , '', '', '', '', '', '', ''];
 let winner = false;
 
 board.style.display = 'none';
@@ -16,7 +15,6 @@ start.addEventListener('click', () => {
     board.style.display = 'grid';
     reset.style.display = 'block';
     start.style.display = 'none';
-    players = 'X';
     text.innerText = "X's Turn";
 
     for (let i = 0; i < squares.length; i++) {
@@ -26,61 +24,59 @@ start.addEventListener('click', () => {
                 x.classList.add('x');
                 squares[i].appendChild(x);
                 x.innerText = 'x';
-                players = 'O';
                 squares[i].style.backgroundColor = 'gray';
+                for(let j = 0; j < spots.length; j++) {
+                    if (i == j) {
+                        spots[j] = players;
+                    }
+                }
+                players = 'O';
                 text.innerText = `${players}'s Turn`;
+                console.log(spots);
+                console.log(winner)
                 checkWinner()
             } else {
                 let o = document.createElement('p');
                 o.classList.add('o');
                 squares[i].appendChild(o);
                 o.innerText = 'o';
-                players = 'X';
                 squares[i].style.backgroundColor = 'gray';
+                for(let j = 0; j < spots.length; j++) {
+                    if (i == j) {
+                        spots[j] = players;
+                    }
+                }
+                players = 'X';
                 text.innerText = `${players}'s Turn`;
+                console.log(spots);
+                console.log(winner)
                 checkWinner()
             }
         })
     }
 })
     
-function checkWinner (players) {
-    // for(let i = 0; i < win.length; i++) {
-    //     let [a, b, c] = win[i];
-    //     if (squares[a].innerText === players && squares[b].innerText === players && squares[c].innerText === players) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
+function checkWinner () {
+    for(let i = 0; i < win.length; i++) {
+        let winConditon = win[i];
+        let square1 = spots[winConditon[0]]
+        let square2 = spots[winConditon[1]]
+        let square3 = spots[winConditon[2]]
 
+        if (square1 == "" || square2 == "" || square3 == "") {
+            continue
+        }
 
-    // for(let i = 0; i < win.length; i++) {
-    //     let winConditon = win[i];
-    //     let square1 = spots[winConditon[0]]
-    //     let square2 = spots[winConditon[1]]
-    //     let square3 = spots[winConditon[2]]
+        if(square1 === square2 && square2 === square3) {
+            winner = true;
+            break;
+        }
+    }
 
-    //     if (square1 == "" || square2 == "" || square3 == "") {
-    //         continue
-    //     }
-
-    //     if(square1 === square2 && square2 === square3) {
-    //         let winner = true
-    //         break;
-    //     }
-    // }
-
-    //     if(winner == true) {
-    //         text.innerText = `${players} wins!`;
-    //         endGame = true;
-    //     }
-
-    //     if (endGame = true) {
-    //         return;
-    //     }
+        if(winner === true) {
+            text.innerText = `${players} wins!`;
+        }
 }
-
 reset.addEventListener('click', () => {
     window.location.reload();
 })
